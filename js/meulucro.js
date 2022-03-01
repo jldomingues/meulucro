5005b024df5a7dffd679ffadbbebc575e74ce087
LucasVS <zelumingues@hotmail.com>
2022-02-28 22:51:47 -0300

    de interno para externo

---
+++ js/meulucro.js
@@ -0,0 +1,57 @@
+
+  function b1(){
+ var pb1=document.getElementById('valor')
+ var pres=document.getElementById('res')
+ var cb1=Number(pb1.value)
+ var lucro=cb1*0.40
+var venda=lucro+cb1
+pres.innerHTML=`Valor de Venda=R$ ${venda.toFixed(2)} <br> Lucro de 40%=R$ ${lucro.toFixed(2)}<br> Faixa de Preço Escolhida:1`
+    
+    
+  }
+    function b2(){
+ var pb1=document.getElementById('valor')
+ var pres=document.getElementById('res')
+ var cb1=Number(pb1.value)
+ var lucro=cb1*0.35
+var venda=lucro+cb1
+pres.innerHTML=`Valor de Venda=R$ ${venda.toFixed(2)} <br> Lucro de 35%=R$ ${lucro.toFixed(2)}<br> Faixa de Preço Escolhida:2`
+    
+    
+    
+  }
+  
+    function b3(){
+ var pb1=document.getElementById('valor')
+ var pres=document.getElementById('res')
+ var cb1=Number(pb1.value)
+ var lucro=cb1*0.30
+var venda=lucro+cb1
+pres.innerHTML=`Valor de Venda =R$ ${venda.toFixed(2)} <br> Lucro de 30%=R$ ${lucro.toFixed(2)}<br> Faixa de Preço Escolhida:3`
+    
+    
+    
+  }
+    function d1(){
+ var pb1=document.getElementById('valor')
+ var pres=document.getElementById('res')
+ var cb1=Number(pb1.value)
+ var lucro=cb1*0.25
+var venda=lucro+cb1
+pres.innerHTML=`Valor de Venda=R$ ${venda.toFixed(2)} <br> Lucro de 25%=R$ ${lucro.toFixed(2)}<br> Faixa de Preço Escolhida:Promoção`
+    
+    
+    
+  }
+    function d2(){
+ var pb1=document.getElementById('valor')
+ var pres=document.getElementById('res')
+ var cb1=Number(pb1.value)
+ var lucro=cb1*0.20
+var venda=lucro+cb1
+pres.innerHTML=`Valor de Venda=R$ ${venda.toFixed(2)} <br> Lucro de 20%=R$ ${lucro.toFixed(2)}
+    <br> Faixa de Preço Escolhida:Saldão`
+    
+    
+   
+  }
